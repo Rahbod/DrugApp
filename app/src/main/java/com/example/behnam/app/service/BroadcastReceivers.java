4668a2c946321c7a63a6e5b51f24ac5524fb274b
Masoud <gharagozlu.masoud@gmail.com>
2018-04-08 19:51:35 +0430

    Reminder (incomplete)

--- app/src/main/java/com/example/behnam/app/service/BroadcastReceivers.java
+++ app/src/main/java/com/example/behnam/app/service/BroadcastReceivers.java
@@ -11,7 +11,7 @@ import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 
-import com.example.behnam.app.ActivityErrorReport;
+import com.example.behnam.app.ActivityReminderDialog;
 
 import java.util.Calendar;
 
@@ -27,83 +27,96 @@ public class BroadcastReceivers extends BroadcastReceiver {
 
         if (intent != null && intent.getAction() != null && !TextUtils.isEmpty(intent.getAction())) {
             String action = intent.getAction();
-            Log.e("masoud", "broadcast execute");
-            if (action.equals(Intent.ACTION_BOOT_COMPLETED) ||
-                    action.equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED) ||
-                    action.equals("BROADCAST_RESTART_APP")) {
+            switch (action) {
+                case Intent.ACTION_BOOT_COMPLETED:
+                case TelephonyManager.ACTION_PHONE_STATE_CHANGED:
+                case "BROADCAST_RESTART_APP":
 //                if (!SessionManager.getExtrasPref(context).getBoolean("ReminderIsStarted")) {
-                    context.startService(new Intent(context, ReminderService.class));
+                    //context.startService(new Intent(context, ReminderService.class));
 //                }
-                Intent intent1 = new Intent(context, ActivityErrorReport.class);
-                context.startActivity(intent1);
+                    Intent intent1 = new Intent(context, ActivityReminderDialog.class);
+                    context.startActivity(intent1);
 
-            } else if (action.equals(Intent.ACTION_TIME_TICK) ||
-                    action.equals(Intent.ACTION_SCREEN_ON)) {
-                //update alarm
-                //updateUtils.update(false);
+                    break;
+                case Intent.ACTION_TIME_TICK:
+                case Intent.ACTION_SCREEN_ON:
+                    //update alarm
+                    //updateUtils.update(false);
 
-            } else if (action.equals(Intent.ACTION_TIME_CHANGED)){
-                //update alarm
-                //updateUtils.update(true);
+                    break;
+                case Intent.ACTION_TIME_CHANGED:
+                    //update alarm
+                    //updateUtils.update(true);
 
-            }else if (action.equals(Intent.ACTION_DATE_CHANGED) ||
-                    action.equals(Intent.ACTION_TIMEZONE_CHANGED)) {
+                    break;
+                case Intent.ACTION_DATE_CHANGED:
+                case Intent.ACTION_TIMEZONE_CHANGED:
 
 //                updateUtils.update(true);
 //                utils.loadApp();
-                LocalBroadcastManager.getInstance(context)
-                        .sendBroadcast(new Intent("LOCAL_INTENT_DAY_PASSED"));
+                    LocalBroadcastManager.getInstance(context)
+                            .sendBroadcast(new Intent("LOCAL_INTENT_DAY_PASSED"));
 
-            } else if (action.equals("BROADCAST_ALARM_FAJR")) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(System.currentTimeMillis());
-                calendar.add(Calendar.MINUTE, 5);
-                int current = (calendar.get(Calendar.HOUR_OF_DAY)*100) + calendar.get(Calendar.MINUTE);
+                    break;
+                case "BROADCAST_ALARM_FAJR": {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTimeInMillis(System.currentTimeMillis());
+                    calendar.add(Calendar.MINUTE, 5);
+                    int current = (calendar.get(Calendar.HOUR_OF_DAY) * 100) + calendar.get(Calendar.MINUTE);
 
-                Integer time = 5;
+                    Integer time = 5;
 
-                if (time < current){
-                    startAthanActivity("FAJR");
-                }
-
-            } else if (action.equals("BROADCAST_ALARM_DHUHR")) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(System.currentTimeMillis());
-                calendar.add(Calendar.MINUTE, 5);
-                int current = (calendar.get(Calendar.HOUR_OF_DAY)*100) + calendar.get(Calendar.MINUTE);
-
-                Integer time = 12;
-
-                if (time != null){
-                    if (time < current){
-                        startAthanActivity("DHUHR");
+                    if (time < current) {
+                        startAthanActivity("FAJR");
                     }
+
+                    break;
                 }
-            } else if (action.equals("BROADCAST_ALARM_MAGHRIB")) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(System.currentTimeMillis());
-                calendar.add(Calendar.MINUTE, 5);
-                int current = (calendar.get(Calendar.HOUR_OF_DAY)*100) + calendar.get(Calendar.MINUTE);
+                case "BROADCAST_ALARM_DHUHR": {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTimeInMillis(System.currentTimeMillis());
+                    calendar.add(Calendar.MINUTE, 5);
+                    int current = (calendar.get(Calendar.HOUR_OF_DAY) * 100) + calendar.get(Calendar.MINUTE);
 
-                Integer time = 19;
+                    Integer time = 12;
 
-                if (time != null){
-                    if (time < current){
-                        startAthanActivity("MAGHRIB");
+                    if (time != null) {
+                        if (time < current) {
+                            startAthanActivity("DHUHR");
+                        }
                     }
+                    break;
                 }
-            } else if(action.equals("notification_cancelled")) {
-                NotificationManager manager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
-                //utils.stop();
-                manager.cancel(AZAN_NOTIFICATION_ID);
+                case "BROADCAST_ALARM_MAGHRIB": {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTimeInMillis(System.currentTimeMillis());
+                    calendar.add(Calendar.MINUTE, 5);
+                    int current = (calendar.get(Calendar.HOUR_OF_DAY) * 100) + calendar.get(Calendar.MINUTE);
+
+                    Integer time = 19;
+
+                    if (time != null) {
+                        if (time < current) {
+                            startAthanActivity("MAGHRIB");
+                        }
+                    }
+                    break;
+                }
+                case "notification_cancelled":
+                    NotificationManager manager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+                    //utils.stop();
+                    assert manager != null;
+                    manager.cancel(AZAN_NOTIFICATION_ID);
+                    break;
             }
         }
     }
 
     private void startAthanActivity(String name) {
-        PowerManager powerManager = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
-        boolean isSceenAwake = (Build.VERSION.SDK_INT < 20? powerManager.isScreenOn():powerManager.isInteractive());
-        if( isSceenAwake) {
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        assert powerManager != null;
+        boolean isSceenAwake = (Build.VERSION.SDK_INT < 20 ? powerManager.isScreenOn() : powerManager.isInteractive());
+        if (isSceenAwake) {
             Log.e("masoud", "azanNotification");
             //utils.azanNotification(true, name);
         } else {
