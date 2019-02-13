d3e041e5ef9c9ba684c9de010d40591c28f38f6f
Masoud <gharagozlu.masoud@gmail.com>
2019-02-13 18:01:15 +0330

    Fix bugs

--- app/src/main/java/ir/rahbod/sinadrug/app/ActivityIndex.java
+++ app/src/main/java/ir/rahbod/sinadrug/app/ActivityIndex.java
@@ -104,7 +104,7 @@ public class ActivityIndex extends AppCompatActivity {
                 setupDate = SessionManager.getExtrasPref(this).getInt("setupDate");
 
             // if installed trial version 10 days ago go to activate page
-            if (activated == 0 && ((int) (System.currentTimeMillis() / 1000) - setupDate) > 600/*864000*/) {
+            if (activated == 0 && ((int) (System.currentTimeMillis() / 1000) - setupDate) > 864000) {
                 SessionManager.getExtrasPref(this).putExtra("selectedVersion", true);
                 Intent intent = new Intent(this, ActivityTrialMessage.class);
                 startActivity(intent);
